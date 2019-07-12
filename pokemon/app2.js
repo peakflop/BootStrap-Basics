e305d6ec217de685ce04c2e7dbabf232141ecb6d
Elizabeth A. Fujita <syp016@c28-0113-18.ad.mtu.edu>
2019-07-12 14:09:52 -0400

    Bootstrap

---
+++ pokemon/app2.js
@@ -0,0 +1,14 @@
+function pikachu(){
+    document.getElementById('pokemon').src="https://media0.giphy.com/media/AFpghpBCYEAwM/giphy.gif";
+}
+function squirtle(){
+    document.getElementById('pokemon').src= "https://media1.giphy.com/media/QzxONYL3xbj6E/giphy.gif";
+    
+}
+function jigglypuff(){
+    document.getElementById('pokemon').src="https://i.gifer.com/Nmqk.gif";
+    
+}
+function piplup(){
+    document.getElementById('pokemon').src="https://media3.giphy.com/media/uLofTbBSg3lQI/source.gif";
+}
